716dbed4ed5c72882fc57a86d5cc608b5f810ea6
tomoki tejika <tomoooki0802@akane.waseda.jp>
2015-11-05 14:42:36 +0900

    鶴田装置加速度センサ

--- StreamReaderForKI/StreamReaderForKI/MainWindow.xaml.cs
+++ StreamReaderForKI/StreamReaderForKI/MainWindow.xaml.cs
@@ -39,8 +39,90 @@ namespace StreamReaderForKI
                 var reader = new BinaryReader(File.OpenRead(this.TextBlock_Main.Text));
                 var size = reader.BaseStream.Length;
                 var writer = new StreamWriter(this.TextBlock_Main.Text.Replace("scd", "csv"));
-                
 
+                if (size < 8)
+                {
+                    throw new Exception("ファイルに正常な書き込みが行われていません。");
+                }
+                //First frame number
+                try
+                {
+                    while (true)
+                    {
+                        //header
+                        var CurrentFrame = reader.ReadUInt32();
+                        writer.Write(CurrentFrame);
+                        writer.Write(",");
+
+                        var time = reader.ReadInt64();
+                        writer.Write(time);
+                        writer.Write(",");
+                        var data = reader.ReadBytes(reader.ReadInt32());
+
+                        string str = BitConverter.ToString(data);
+
+                        if (str != null)
+                        {    //改行コード取り除き
+                            var buf = str.Split(new char[] { ',', '\t' });
+
+                            try
+                            {
+                                var timer = float.Parse(buf[0]);
+                                //analog1 = (float(buf[1])-f0[0]);
+                                writer.Write(timer);
+
+                                var analog1 = 0.5096 * Math.Exp(0.0048 * (float.Parse(buf[1]) - f0[0]));
+                                var aX = int.Parse(buf[2]);
+                                var aY = int.Parse(buf[3]);
+                                var aZ = int.Parse(buf[4]);
+                                var gX = int.Parse(buf[5]);
+                                var gY = int.Parse(buf[6]);
+                                var gZ = int.Parse(buf[7]);
+                                //mX = int(buf[8]);
+                                //mY = int(buf[9]);
+                                //mZ = int(buf[10]);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+
+
+                     
+                        writer.Write("\n");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    writer.Write(ex.Message);
+                }
+                finally
+                {
+                    reader.Close();
+                    writer.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Border_Main_DragEnter(object sender, DragEventArgs e)
+        {
+            dataObj = e.Data as IDataObject;
+            String[] FileName = dataObj.GetData(DataFormats.FileDrop) as String[];
+            this.TextBlock_Main.Text = FileName[0];
+
+        }
+    }
+}
+
+
+/*
                 if (size < 8)
                 {
                     throw new Exception("ファイルに正常な書き込みが行われていません。");
@@ -143,3 +225,4 @@ namespace StreamReaderForKI
         }
     }
 }
+*/
